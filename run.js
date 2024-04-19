293ce669efed56c44ec0080457aa7fadb87e5082
Meer Hashaam Khan <hashaamkhan975@gmail.com>
2024-04-20 02:02:45 +0500

    Add TypeScript configuration

--- run.js
+++ run.js
@@ -1,20 +1,27 @@
-const satellite = require('./src/satellite')
-const iridium = require('./src/iridium')
+// ES6 import statements
+import satellite from './src/satellite.js';
+import iridium from './src/iridium.js';
 
-const location = [39.9042, 116.4074, '%E5%8C%97%E4%BA%AC%E5%B8%82', 52, 'ChST']
-// COOKIE需要先通过浏览器调到中文
+// It appears that 'location' is declared but not used anywhere within this file.
+// If 'location' is not used, it should be removed or commented out unless it's for future use.
+const location = [39.9042, 116.4074, '%E5%8C%97%E4%BA%AC%E5%B8%82', 52, 'ChST'];
 
+// Uncomment this if 'names' will be used later or remove it if it's not needed.
 // const names = ["ISS", "IridiumFlares"];
-// https://www.heavens-above.com/PassSummary.aspx?satid=41765&lat=0&lng=0&loc=Unspecified&alt=0&tz=UCT
 
+// Running the getTable function for the satellite module
 satellite.getTable({
-  target: 25544,
-  pages: 4,
-  root: './public/data/'
-}) // ISS
+  target: 25544,  // Example satellite ID for ISS
+  pages: 4,       // Number of pages to fetch
+  root: './public/data/'  // Directory to store data
+});
+
+// Uncomment the below code to run the getTable function for the iridium module if needed
 /*
 iridium.getTable({
-	pages: 4,
-	root: "./public/data/"
+  pages: 4,       // Number of pages to fetch
+  root: "./public/data/"  // Directory to store data
 });
 */
+
+// Note: Ensure that the `satellite` and `iridium` modules are properly exporting a `getTable` function.
--- src/iridium.js
+++ src/iridium.js
@@ -1,53 +1,55 @@
-const request = require('request')
-const cheerio = require('cheerio')
-const fs = require('fs')
-const utils = require('./utils')
+import fs from 'fs';
+import request from 'request';
+import cheerio from 'cheerio';
+import utils from './utils.js';
 
-const eventsIridium = ['brightness', 'altitude', 'azimuth', 'satellite', 'distanceToFlareCentre', 'brightnessAtFlareCentre', 'date', 'time', 'distanceToSatellite', 'AngleOffFlareCentre-line', 'flareProducingAntenna', 'sunAltitude', 'angularSeparationFromSun', 'image', 'id']
+const eventsIridium = ['brightness', 'altitude', 'azimuth', 'satellite', 'distanceToFlareCentre', 'brightnessAtFlareCentre', 'date', 'time', 'distanceToSatellite', 'AngleOffFlareCentre-line', 'flareProducingAntenna', 'sunAltitude', 'angularSeparationFromSun', 'image', 'id'];
 
-function getTable (config) {
-  let database = config.database || []
-  let counter = config.counter || 0
-  const opt = config.opt || 0
-  const basedir = config.root + 'IridiumFlares/'
+function getTable(config) {
+  let database = config.database || [];
+  let counter = config.counter || 0;
+  let options;
+  const opt = config.opt || '';
+  const basedir = `${config.root}IridiumFlares/`;
+
   if (counter === 0) {
-    options = utils.get_options('IridiumFlares.aspx?')
+    options = utils.get_options('IridiumFlares.aspx?');
     if (!fs.existsSync(basedir)) {
       fs.mkdir(basedir, (err) => {
-        if (err) console.log(err)
-      })
+        if (err) console.error('Directory creation error:', err);
+      });
     }
   } else {
-    options = utils.post_options('IridiumFlares.aspx?', opt)
+    options = utils.post_options('IridiumFlares.aspx?', opt);
   }
+
   request(options, (error, response, body) => {
-    if (error || response.statusCode !== 200) return
-    const $ = cheerio.load(body, {
-      decodeEntities: false
-    })
-    let next = '__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS='
-    const tbody = $('form').find('table.standardTable tbody')
-    const queue = []
+    if (error || response.statusCode !== 200) return;
+    const $ = cheerio.load(body, { decodeEntities: false });
+    let next = '__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=';
+    const tbody = $('form').find('table.standardTable tbody');
+    const queue = [];
+
     tbody.find('tr').each((i, o) => {
-      const temp = {}
-      for (let i = 0; i < 6; i++) {
-        temp[eventsIridium[i]] = $(o).find('td').eq(i + 1).text()
+      const temp = {};
+      for (let j = 0; j < 6; j++) {
+        temp[eventsIridium[j]] = $(o).find('td').eq(j + 1).text();
       }
-      temp.url = 'https://www.heavens-above.com/' + $(o).find('td').eq(0).find('a').attr('href').replace('type=V', 'type=A')
-      queue.push(temp)
-    })
-    async function factory (temp) {
-      return await new Promise((resolve, reject) => {
+      temp.url = `https://www.heavens-above.com/${$(o).find('td').eq(0).find('a').attr('href').replace('type=V', 'type=A')}`;
+      queue.push(temp);
+    });
+
+    async function factory(temp) {
+      return new Promise((resolve, reject) => {
         request(utils.iridium_options(temp.url), (error, response, body) => {
-          if (error || response.statusCode !== 200) { // 在无SessionID时返回500
-            reject(error)
-            return
+          if (error || response.statusCode !== 200) {
+            reject(error);
+            return;
           }
-          console.log('Success', temp)
-          const $ = cheerio.load(body, {
-            decodeEntities: false
-          })
-          const table = $('form').find('table.standardTable'); const tr = table.find('tbody tr');
+          console.log('Success:', temp);
+          const $ = cheerio.load(body, { decodeEntities: false });
+          const table = $('form').find('table.standardTable');
+          const tr = table.find('tbody tr');
           [
             [6, 0],
             [7, 1],
@@ -57,32 +59,34 @@ function getTable (config) {
             [11, 10],
             [12, 11]
           ].forEach((ele) => {
-            temp[eventsIridium[ele[0]]] = tr.eq(ele[1]).find('td').eq(1).text()
-          })
-          temp[eventsIridium[13]] = 'https://www.heavens-above.com/' + $('#ctl00_cph1_imgSkyChart').attr('src') // .replace("size=800", "size=1600");,
-          const id = utils.md5(Math.random().toString()) // temp[eventsIridium[6]];
-          temp[eventsIridium[14]] = id
-          fs.appendFile(basedir + id + '.html', table.html(), (err) => {
-            if (err) console.log(err)
-          }) // 保存表格
-          request.get(utils.image_options(temp[eventsIridium[13]])).pipe(fs.createWriteStream(basedir + id + '.png', {
+            temp[eventsIridium[ele[0]]] = tr.eq(ele[1]).find('td').eq(1).text();
+          });
+          temp[eventsIridium[13]] = `https://www.heavens-above.com/${$('#ctl00_cph1_imgSkyChart').attr('src')}`;
+          const id = utils.md5(Math.random().toString());
+          temp[eventsIridium[14]] = id;
+          fs.appendFile(`${basedir}${id}.html`, table.html(), (err) => {
+            if (err) console.error('File writing error:', err);
+          });
+          request.get(utils.image_options(temp[eventsIridium[13]])).pipe(fs.createWriteStream(`${basedir}${id}.png`, {
             flags: 'a'
           })).on('error', (err) => {
-            console.error(err)
-          }) // 下载图片
-          resolve(temp)
-        })
-      })
+            console.error('Image download error:', err);
+          });
+          resolve(temp);
+        });
+      });
     }
-    Promise.allSettled(queue.map(async temp => await factory(temp))).then(results => {
-      results = results.filter(p => p.status === 'fulfilled').map(p => p.value)
-      database = database.concat(results)
+
+    Promise.allSettled(queue.map(temp => factory(temp))).then(results => {
+      results = results.filter(p => p.status === 'fulfilled').map(p => p.value);
+      database = database.concat(results);
       $('form').find('input').each((i, o) => {
-        if ($(o).attr('name') === 'ctl00$cph1$btnPrev' || $(o).attr('name') === 'ctl00$cph1$visible') return
-        else next += `&${$(o).attr('name')}=${$(o).attr('value')}`
-      })
-      next += '&ctl00$cph1$visible=radioVisible'
-      next = next.replace(/\+/g, '%2B').replace(/\//g, '%2F') // .replace(/\$/g, "%24");
+        if ($(o).attr('name') !== 'ctl00$cph1$btnPrev' && $(o).attr('name') !== 'ctl00$cph1$visible') {
+          next += `&${$(o).attr('name')}=${$(o).attr('value')}`;
+        }
+      });
+      next += '&ctl00$cph1$visible=radioVisible';
+      next = next.replace(/\+/g, '%2B').replace(/\//g, '%2F');
       if (counter++ < config.pages) {
         getTable({
           count: config.count,
@@ -91,14 +95,14 @@ function getTable (config) {
           counter,
           opt: next,
           database
-        })
+        });
       } else {
-        fs.appendFile(basedir + 'index.json', JSON.stringify(database), (err) => {
-          if (err) console.log(err)
-        })
+        fs.appendFile(`${basedir}index.json`, JSON.stringify(database), (err) => {
+          if (err) console.error('Final JSON writing error:', err);
+        });
       }
-    })
-  })
+    });
+  });
 }
 
-exports.getTable = getTable
+export { getTable };
